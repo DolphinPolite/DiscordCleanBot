0405256f2b9ac7fec96b8ca3d916eb2bb3ddefb2
DolphinPolite <137199618+DolphinPolite@users.noreply.github.com>
2024-09-14 22:25:30 +0300

    Update function.py

--- function.py
+++ function.py
@@ -2,12 +2,40 @@ import discord
 from discord import Embed
 from discord.ui import Button, View
 from discord.ext import commands, tasks
-from itertools import cycle 
-from dotenv import load_dotenv
-import os
+import random
 
-class Cleaner():
+class Cleaner:
     def __init__(self):
+        self.Recommendations = ["öneri1","öneri2","öneri3"]
+    
+    async def YesCallBack(self, interaction: discord.Interaction):
+        await interaction.followup.send("Glad to hear that!", ephemeral=True, view=self.GetAnotherView())
+
+    async def NoCallback(self, interaction: discord.Interaction):
+        await interaction.followup.send(self.GetARecommendation(), ephemeral=True, view=self.GetYesOrNoView())    
+
+    async def ThanksCallback(self, interaction: discord.Interaction):
+        await interaction.followup.send("Yor'ue welcome", ephemeral=True)   
+
+    async def GetAnother(self, interaction: discord.Interaction):
+        await interaction.followup.send("Glad to hear that!", ephemeral=True, view=self.GetYesOrNoView())
+
+    def ButtonCallbackMatcher(self, buttonList: list, name: str, View:View):
+        for i in range(len(buttonList)):
+            callback = ""
+
+            match name:
+                case "Yes":
+                    callback = self.YesCallBack()
+                case "No":
+                    callback = self.NoCallback()
+                case "Thank you!":
+                    callback = self.ThanksCallback()
+                case "Get Another Recommendation!":
+                    callback = self.GetAnother()
+
+            buttonList[i].callback = callback
+            View.add_item(buttonList[i])
 
     def ButtonListMaker(self, length:int, names:list, styles:list, emojis:list = None) -> list:
         buttonList = []
@@ -18,26 +46,43 @@ class Cleaner():
 
         return buttonList
 
-    def ButtonCallbackMatcher(self, buttonList: list, callbackList: list, View:View):
-        for i in range(len(buttonList)):
-            buttonList[i].callback = callbackList[i]
-            View.add_item(buttonList[i])
+    def GetARecommendation(self):
+        recommendation = random.choice(self.Recommendations)
 
-    def GetView() -> View:
+        return recommendation
+
+    def GetYesOrNoView(self) -> View:
         view = View
 
         names = []
         emojis = []
         styles = []
-        callbackNameList = []
 
-        names.append("Shield"); emojis.append("🛡️"), styles.append(discord.ButtonStyle.blurple); callbackNameList.append("Feature")
-        names.append("Break Shield"); emojis.append("🤜"), styles.append(discord.ButtonStyle.blurple); callbackNameList.append("Feature")
-        names.append("Repair Shield"); emojis.append("🔨"), styles.append(discord.ButtonStyle.blurple); callbackNameList.append("Feature")
-        names.append("Extra Damage"); emojis.append("🗡️"), styles.append(discord.ButtonStyle.blurple); callbackNameList.append("Feature")
-        names.append("See"); emojis.append("👀"), styles.append(discord.ButtonStyle.blurple); callbackNameList.append("Feature")
-        names.append("Exit"); emojis.append("👀"), styles.append(discord.ButtonStyle.red); callbackNameList.append("Exit")
+        names.append("Yes"); emojis.append("👍"), styles.append(discord.ButtonStyle.success)
+        names.append("No"); emojis.append("👎"), styles.append(discord.ButtonStyle.red)
 
-        ButtonList = self.ButtonListMaker(length=12, names=names, emojis=emojis, styles=styles)      
+        ButtonList = self.ButtonListMaker(length=2, names=names, emojis=emojis, styles=styles)      
 
-        ButtonCallbackMatcherForGame(buttonList=ButtonList, callbackNameList=callbackNameList, View=view)
+        self.ButtonCallbackMatcher(buttonList=ButtonList, name=names, view=view)
+
+        return view
+    
+    def GetAnotherView(self) -> View:
+        view = View
+
+        names = []
+        emojis = []
+        styles = []
+
+        names.append("Get Another Recommendation!"); emojis.append("✅"), styles.append(discord.ButtonStyle.success)
+        names.append("Thank you!"); emojis.append("👌"), styles.append(discord.ButtonStyle.gray)
+
+        ButtonList = self.ButtonListMaker(length=2, names=names, emojis=emojis, styles=styles)      
+
+        self.ButtonCallbackMatcher(buttonList=ButtonList, name=names, view=view)
+
+        return view
+    
+async def Starter(interaction: discord.Interaction):
+    cleaner = Cleaner()
+    await interaction.followup.send(cleaner.GetARecommendation(), ephemeral=True, view=cleaner.GetYesOrNoView())
